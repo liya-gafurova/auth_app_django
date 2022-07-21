cbf1f94114fa78d33076789fd54f71df75077ad5
liya-gafurova <gali4086555@gmail.com>
2022-07-21 15:05:52 +0600

    token auth

--- djanho_rest_auth_auth/urls.py
+++ djanho_rest_auth_auth/urls.py
@@ -1,6 +1,9 @@
-from django.urls import re_path, include
+from django.urls import re_path, include, path
+
+from djanho_rest_auth_auth.views import TestView
 
 rest_auth_urlpatterns = [
     re_path(r'^dj-rest-auth/', include('dj_rest_auth.urls')),
     re_path(r'^dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('dj-rest-auth/test/', TestView.as_view(), name='test-path')
 ]
--- djanho_rest_auth_auth/views.py
+++ djanho_rest_auth_auth/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import serializers
+from rest_framework.fields import CharField
+from rest_framework.generics import GenericAPIView, RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+DATA = [
+    {'name': "cat", 'color': "red"},
+    {'name': "cat", 'color': "red"},
+    {'name': "cat", 'color': "red"}
+]
+
+class TestDataSerializer(serializers.Serializer):
+    name = CharField()
+    color = CharField()
+
+class TestView(RetrieveAPIView):
+    permission_classes = (IsAuthenticated, )
+    serializer_class = TestDataSerializer
+
+    def get_object(self):
+        return DATA[0]
+
+    def get(self, request, *args, **kwargs):
+        return super(TestView, self).get(request, *args, **kwargs)
